a7b49fce075ab2e9d1f3e982246a0bddd4f63bdd
HodeonArtz <martidicianolold@gmail.com>
2025-01-30 19:13:24 +0100

    feat: implement changing color to links

--- src/script.js
+++ src/script.js
@@ -1,24 +1,43 @@
-function useChromeFunction(func) {
-  return () => {
+/**
+ *
+ * @param {function} func
+ * @param {array} args
+ * @returns {function}
+ */
+function useChromeFunction(func, args) {
+  return () =>
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (!tabs.length) return;
 
       chrome.scripting.executeScript({
         target: { tabId: tabs[0].id },
         func,
+        args,
       });
     });
-  };
 }
-
-// <<===========||===========||===========||===========>>
+// >>=====>>====>>====#[<| EFFECTS |>]#====<<====<<=====<<
 
 const setBackgroundRedButton = document.querySelector(".set-bg-btn");
+
+const setBackgroundRed = () =>
+  (document.body.style.backgroundColor = "#883333");
 
 setBackgroundRedButton.addEventListener(
   "click",
   useChromeFunction(setBackgroundRed)
 );
 
-const setBackgroundRed = () =>
-  (document.body.style.backgroundColor = "#883333");
+// <<===========||===========||===========||===========>>
+
+const setLinkColorButton = document.querySelector(".set-link-color");
+const linkColorInput = document.querySelector(".link-color-input");
+
+function setLinkColor(color) {
+  const allLinks = document.querySelectorAll("a");
+  allLinks.forEach((link) => (link.style.color = color));
+}
+
+setLinkColorButton.addEventListener("click", () => {
+  useChromeFunction(setLinkColor, [linkColorInput.value])();
+});
